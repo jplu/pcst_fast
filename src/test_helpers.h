2173abc023005d6e58a836bb4b0d5cb0e55eaf48
Julien Plu <plu.julien@gmail.com>
2025-03-30 23:44:56 +0200

    Turn test_helpers.h to C++23

--- src/test_helpers.h
+++ src/test_helpers.h
@@ -1,29 +1,35 @@
-#ifndef __TEST_HELPERS_H__
-#define __TEST_HELPERS_H__
+#pragma once
 
 #include <cstdio>
 #include <vector>
+#include <concepts>
 
 #include "gtest/gtest.h"
 
-template <typename T, size_t N>
-T* begin(T(&arr)[N]) { return &arr[0]; }
-template <typename T, size_t N>
-T* end(T(&arr)[N]) { return &arr[0]+N; }
+namespace test_helpers {
+    template <typename T, size_t N> requires std::equality_comparable<T>
+    constexpr T* begin(T(&arr)[N]) noexcept { return &arr[0]; }
+    template <typename T, size_t N> requires std::equality_comparable<T>
+    constexpr T* end(T(&arr)[N]) noexcept { return &arr[0] + N; }
 
-void WriteToStderr(const char* s) {
-  fprintf(stderr, "%s", s);
-  fflush(stderr);
+    inline void WriteToStderr(const char* s) {
+      std::fprintf(stderr, "%s", s);
+      std::fflush(stderr);
+    }
+
+    template <typename T> requires std::equality_comparable_with<T, T>
+    void CheckResult(const std::vector<T>& expected_result,
+                     const std::vector<T>& result) {
+        ASSERT_EQ(expected_result.size(), result.size())
+            << "Result size mismatch. Expected: " << expected_result.size()
+            << ", Got: " << result.size();
+
+        for (size_t ii = 0; ii < expected_result.size(); ++ii) {
+            EXPECT_EQ(expected_result[ii], result[ii])
+                << "Element mismatch at index " << ii
+                << ". Expected: " << expected_result[ii]
+                << ", Got: " << result[ii];
+        }
+    }
+
 }
-
-template <typename T>
-void CheckResult(const std::vector<T>& expected_result,
-                 const std::vector<T>& result) {
-  ASSERT_EQ(expected_result.size(), result.size());
-  for (size_t ii = 0; ii < expected_result.size(); ++ii) {
-    EXPECT_EQ(expected_result[ii], result[ii])
-        << "Not matching at index " << ii;
-  }
-}
-
-#endif
