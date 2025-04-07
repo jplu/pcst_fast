8e2e91590bb998006b1db2a18003ae95c69a4f07
Julien Plu <plu.julien@gmail.com>
2025-04-07 13:18:59 +0200

    Update no_pruner.cc

--- src/no_pruner.cc
+++ src/no_pruner.cc
@@ -1,5 +1,6 @@
 #include "no_pruner.h"
 #include "pruning_context.h"
+#include "logger.h"
 
 #include <vector>
 #include <numeric>
@@ -7,13 +8,15 @@
 namespace cluster_approx {
     namespace internal {
 
+        using internal::LogLevel;
+
         void NoPruner::prune(const PruningContext& context,
                     std::vector<PCSTFast::IndexType>& result_nodes,
                     std::vector<PCSTFast::IndexType>& result_edges) {
-            context.log(3, "Pruning: None. Using Phase 1 result directly.\n");
+            context.logger.log(LogLevel::INFO, "Pruning: None. Using Phase 1 result directly.\n");
             result_edges.assign(context.phase1_result.begin(), context.phase1_result.end());
             build_node_set_base(context, result_edges, result_nodes);
-            context.log(3, "Final Result (No Pruning): Nodes=%zu, Edges=%zu\n", result_nodes.size(), result_edges.size());
+            context.logger.log(LogLevel::INFO, "Final Result (No Pruning): Nodes=%zu, Edges=%zu\n", result_nodes.size(), result_edges.size());
         }
 
     }
